9cce823fa267ad51232c5387cde8f8ff14cfffd9
murukai <murukai@murukai-ThinkPad-X230>
2019-08-25 13:33:48 +0200

    #comment - Added 1. home page 2. Added controller 3. Added bootstrap, jquery, popper, fontawesome dependencies

--- to-do-app/src/main/java/com/afrikatek/todoapp/controller/ToDoController.java
+++ to-do-app/src/main/java/com/afrikatek/todoapp/controller/ToDoController.java
@@ -1,9 +1,14 @@
 package com.afrikatek.todoapp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ToDoController {
 
-	
+	@GetMapping(value = {"/","/home","index"})
+	public String home(Model model) {
+		return "home";
+	}
 }
